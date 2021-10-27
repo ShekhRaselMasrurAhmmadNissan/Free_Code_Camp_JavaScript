556275e68b287139395976e77ae6ea497cd7712a
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-10-27 21:28:54 +0600

    Switch Statement in JS..

---
+++ switchStatement/app.js
@@ -0,0 +1,26 @@
+let dice = 4
+
+switch (dice) {
+	case 1:
+		console.log(`Better luck next time. The lucky number is ${dice}`)
+		break;
+	case 2:
+		console.log(`Better luck next time. The lucky number is ${dice}`)
+		break;
+	case 3:
+		console.log(`Better luck next time. The lucky number is ${dice}`)
+		break;
+	case 4:
+		console.log(`Congratulation... You have won the Jackpot.`)
+		break;
+	case 5:
+		console.log(`Better luck next time. The lucky number is ${dice}`)
+		break;
+	case 6:
+		console.log(`Better luck next time. The lucky number is ${dice}`)
+		break;
+
+	default:
+		console.log(`You didn't roll the dice.`)
+		break;
+}
