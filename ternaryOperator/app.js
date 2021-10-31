1513acba50a860e4288bbc15ba29b06e9510d4dd
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-10-31 15:24:47 +0600

    Ternary Operator in JS...

---
+++ ternaryOperator/app.js
@@ -0,0 +1,20 @@
+// Unary Operators
+let text = `Text`
+console.log(typeof (text));
+
+// Binary Operators
+let number = 5 + 7
+let number2 = 6 + 9
+console.log(number + number)
+
+// Ternary Operators
+
+const value = (2 > 1)
+
+// if (value) {
+// 	console.log(`The condition is True`)
+// } else {
+// 	console.log(`The condition is False`)
+// }
+
+value ? console.log(`The condition is True`) : console.log(`The Condition is False`)
