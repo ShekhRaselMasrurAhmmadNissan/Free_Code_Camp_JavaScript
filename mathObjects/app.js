821a4b3d76422b92084d00a94cd2e31806db30e8
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-11-04 11:52:09 +0600

    Math Objects in JS.

---
+++ mathObjects/app.js
@@ -0,0 +1,24 @@
+/*
+** Math - Standards built-in objects
+** Always available
+*/
+let number = 0
+let result = 0
+// number = 4.5684
+// result = Math.floor(number)
+
+// number = 4.5684
+// result = Math.ceil(number)
+
+// number = 25
+// result = Math.sqrt(number)
+
+// result = Math.PI
+
+// result = Math.min(54, 65 ,78 ,20 ,98 ,74 ,101)
+
+// result = Math.max(54, 65 ,78 ,20 ,98 ,74 ,101)
+
+result = Math.ceil(Math.random()*10)
+
+console.log(result)
