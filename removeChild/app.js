794f26db9dc87dba1f6c34117f2ed92db5c062a3
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-11-08 13:47:13 +0600

    Remove and Remove Child methods in JS.

---
+++ removeChild/app.js
@@ -0,0 +1,9 @@
+// Remove
+// Remove Child
+
+const result = document.querySelector('#result')
+// result.remove()
+
+const heading = result.querySelector('h1')
+console.log(heading)
+result.removeChild(heading)
