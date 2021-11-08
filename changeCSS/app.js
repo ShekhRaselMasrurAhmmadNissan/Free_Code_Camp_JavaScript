005bcc6d322503161a6f2f624754dce3dffbfd49
ShekhRaselMasrurAhmmadNissan <shekhraselmasrurahmmadnissan@gmail.com>
2021-11-08 20:59:51 +0600

    Changing Style using CSS Property in JS.

---
+++ changeCSS/app.js
@@ -0,0 +1,10 @@
+// CSS
+const randomDiv = document.querySelector('.random')
+// randomDiv.style.background = 'blue'
+// randomDiv.style.color = 'white'
+// randomDiv.style.fontSize = '3rem'
+// randomDiv.style.textTransform = 'capitalize'
+
+// console.log(randomDiv.textContent)
+
+randomDiv.classList.add('title')
